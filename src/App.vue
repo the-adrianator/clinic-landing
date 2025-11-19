ec4a85a35ea9a085dcbc7c75752161550f1405c2
Adrian <adrian@theround.com>
2025-11-19 11:41:12 +0000

    feat: add Specialists component to showcase expert team
    - Create new Specialists.vue component with team member cards
    - Display specialist profiles with images, ratings, and specialities
    - Add call-to-action section for consultations
    - Integrate Specialists component into App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Hero />
     <Services />
     <About />
+    <Specialists />
   </div>
 </template>
 
@@ -13,6 +14,7 @@ import Header from '@components/Header.vue'
 import Hero from '@components/Hero.vue'
 import Services from '@components/Services.vue'
 import About from '@components/About.vue'
+import Specialists from '@components/Specialists.vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
